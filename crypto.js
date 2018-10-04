f69134fbdc67ab71a197b591ce0999a0c46f7d92
osama <osama@mxd3d.com>
2018-10-04 20:05:03 +0300

    push to heroku

---
+++ crypto.js
@@ -0,0 +1,25 @@
+// Nodejs encryption with CTR
+var crypto = require("crypto"),
+  algorithm = "aes-256-ctr",
+  password = "5a6b9c8d0";
+
+var encryption = {
+  encrypt: decryptedStr => {
+    // console.log("decrypted text is: ", decryptedStr);
+    var cipher = crypto.createCipher(algorithm, password);
+    var crypted = cipher.update(decryptedStr, "utf8", "hex");
+    crypted += cipher.final("hex");
+
+    return crypted;
+  },
+  decrypt: encryptedStr => {
+    // console.log("encrypted text is: ", encryptedStr);
+    var decipher = crypto.createDecipher(algorithm, password);
+    var dec = decipher.update(encryptedStr, "hex", "utf8");
+    dec += decipher.final("utf8");
+
+    return dec;
+  }
+};
+
+module.exports = encryption;
---
+++ index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var encryption = require('./crypto');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
+app.get('/', function(req, res){
+  res.send('There is nothing to show you here.');
+});
+
+app.post ('/encrypt', (req, res) => {
+    res.send(encryption.encrypt(req.body.originText));
+});
+
+app.post ('/encrypt', (req, res) => {
+    res.send(encryption.decrypt(req.body.encrypted));
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log ("Server is connected on port:", port)
+});
